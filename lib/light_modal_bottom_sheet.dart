8b3a7157b643355b2a4ec20458f9f0f38bb642f5
amirhome <amir.email@yahoo.com>
2023-02-07 13:48:52 +0300

    init

---
+++ lib/light_modal_bottom_sheet.dart
@@ -0,0 +1,7 @@
+library light_modal_bottom_sheet;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/amir/snap/flutter/common/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/amir/Codes/AndroidStudioProjects/amirhome.com/light_modal_bottom_sheet"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.0.1"
+export "FLUTTER_BUILD_NUMBER=0.0.1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
